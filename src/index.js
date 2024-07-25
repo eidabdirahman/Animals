483fa837e57cee6862aadeadd55296b686768502
Eid Abdirahman <140173086+eidabdirahman@users.noreply.github.com>
2024-07-25 21:18:13 +0300

    animals

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import reactDOM from 'react-dom/client';
+import App from './App';
+
+const el = document.getElementById('root');
+const root = reactDOM.createRoot(el);
+
+root.render(<App/>);
